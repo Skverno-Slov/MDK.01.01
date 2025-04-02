4a8418753f3484950e0fb9b3d4e7a5f26d30dd9b
Zakhar Lukanin <zaharlukanin19@gmail.com>
2025-04-03 00:29:14 +0300

    закинул шаблон

---
+++ PractWork3/PractWork3/CreateWordDocumentForm.cs
@@ -0,0 +1,23 @@
+﻿namespace PractWork3
+{
+    public partial class CreateWordDocumentForm : Form
+    {
+        WordMeneger meneger = new();
+
+        public CreateWordDocumentForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CreateDocumentButton_Click(object sender, EventArgs e)
+        {
+            meneger.OverrideTemplateDocument(InputTextBox.Text, (int)InputTaskCountNumericUpDown.Value);
+        }
+
+        private void CreateNewDocumentButton_Click(object sender, EventArgs e)
+        {
+            meneger.CreateNewDocument(InputTextBox.Text, (int)InputTaskCountNumericUpDown.Value);
+        }
+    }
+}
